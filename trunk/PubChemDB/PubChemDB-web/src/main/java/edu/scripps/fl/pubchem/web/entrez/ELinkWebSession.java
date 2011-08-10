7310a667f7bd86be5e60a0aa3a4d0ef58bea55ae
southern@scripps.edu <southern@scripps.edu@65f7548a-6e78-4f9b-2e77-0b6265fc650d>
2011-08-10 18:46:28 +0000

    Fix sax parsing

--- trunk/PubChemDB/PubChemDB-web/src/main/java/edu/scripps/fl/pubchem/web/entrez/ELinkWebSession.java
+++ trunk/PubChemDB/PubChemDB-web/src/main/java/edu/scripps/fl/pubchem/web/entrez/ELinkWebSession.java
@@ -1,28 +1,40 @@
-/*
- * Copyright 2011 The Scripps Research Institute
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*					
+ * Copyright 2011 The Scripps Research Institute					
+ *					
+ * Licensed under the Apache License, Version 2.0 (the "License");					
+ * you may not use this file except in compliance with the License.					
+ * You may obtain a copy of the License at					
+ *					
+ *     http://www.apache.org/licenses/LICENSE-2.0					
+ *					
+ * Unless required by applicable law or agreed to in writing, software					
+ * distributed under the License is distributed on an "AS IS" BASIS,					
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.					
+ * See the License for the specific language governing permissions and					
+ * limitations under the License.					
  */
 package edu.scripps.fl.pubchem.web.entrez;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.apache.commons.lang.StringUtils;
 import org.dom4j.Document;
 import org.dom4j.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import edu.scripps.fl.pubchem.web.ELinkResult;
 import edu.scripps.fl.pubchem.web.session.WebSessionBase;
@@ -30,11 +42,24 @@ import edu.scripps.fl.util.CollectionUtils;
 
 public class ELinkWebSession extends WebSessionBase {
 
+	public static ELinkWebSession newInstance(String dbFrom, String db, List<String> linkNames, List<Long> ids, String term) throws Exception {
+		ELinkWebSession session = new ELinkWebSession();
+		session.setDbFrom(dbFrom);
+		session.setDb(db);
+		session.setLinkName(StringUtils.join(linkNames, ","));
+		session.setIds(ids);
+		session.setTerm(term);
+		return session;
+	}
+
+	private static final Logger log = LoggerFactory.getLogger(ELinkWebSession.class);
 	private Long id;
 	private String dbFrom;
 	private String db;
 	private String linkName;
 	private Collection<Long> ids;
+	private String term;
+	private List<ELinkResult> results;
 
 	public String getDbFrom() {
 		return dbFrom;
@@ -55,20 +80,33 @@ public class ELinkWebSession extends WebSessionBase {
 	public String getLinkName() {
 		return linkName;
 	}
-	
+
+	public String getTerm() {
+		return term;
+	}
+
 	public Map<Long, List<ELinkResult>> getELinkResultsAsMap() throws Exception {
-		Collection<ELinkResult> results = getELinkResults();
-		return CollectionUtils.toMap("id", results);
+		return CollectionUtils.toMap("id", getELinkResults());
+	}
+
+	public List<ELinkResult> getELinkResults() throws Exception {
+		return results;
+	}
+
+	public Set<Long> getAllIds(String linkName) throws Exception {
+		Set<Long> ids = new HashSet<Long>();
+		log.info("Fetching ids from elink session.");
+		for (ELinkResult result : getELinkResults()) {
+			List<Long> idList = result.getIds(db, linkName);
+			if (null != idList)
+				ids.addAll(idList);
+		}
+		log.info("Retrieved ids from elink session.");
+		return ids;
 	}
 	
-
-	public Collection<ELinkResult> getELinkResults() throws Exception {
-		return getELinkResults(new ArrayList<ELinkResult>());
-	}
-
-	@SuppressWarnings("unchecked")
-	public Collection<ELinkResult> getELinkResults(Collection<ELinkResult> relations) throws Exception {
-		List<Object> params = new ArrayList<Object>(6 + 2 * getIds().size());
+	protected List<Object> getParams() {
+		List<Object> params = new ArrayList<Object>(8 + 2 * getIds().size());
 		params.addAll(Arrays.asList(new String[] { "dbfrom", getDbFrom(), "db", getDb() }));
 		if( null != getLinkName() && ! "".equals(getLinkName()) ) {
 			params.add("linkname");
@@ -78,8 +116,25 @@ public class ELinkWebSession extends WebSessionBase {
 			params.add("id");
 			params.add(id.longValue());
 		}
-		Document doc = EUtilsFactory.getInstance().getDocument("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi", params);
+		if(null != getTerm() && ! "".equals(getTerm())){			
+			params.add("term");		
+			params.add(getTerm());		
+		}
+		params.add("version");
+		params.add("2.0");
+		return params;
+	}
 
+	@SuppressWarnings("unchecked")
+	public List<ELinkResult> run() throws Exception {
+		getELinkResultsSAX(results = new ArrayList(1000));
+		return results;
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Collection<ELinkResult> getELinkResultsDOM(Collection<ELinkResult> relations) throws Exception {
+		Document doc = EUtilsFactory.getInstance().getDocument("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi", getParams());
+		log.info("Parsing elink results using DOM.");
 		List<Node> linkSets = doc.selectNodes("/eLinkResult/LinkSet");
 		if (relations instanceof ArrayList)
 			((ArrayList<ELinkResult>) relations).ensureCapacity(linkSets.size());
@@ -104,7 +159,86 @@ public class ELinkWebSession extends WebSessionBase {
 			}
 			relations.add(rel);
 		}
+		log.info("Finished parsing elink results.");
 		return relations;
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Collection<ELinkResult> getELinkResultsSAX(final Collection<ELinkResult> relations) throws Exception {
+		log.info("Memory in use before inputstream: " + memUsage());
+		InputStream is = EUtilsFactory.getInstance().getInputStream("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi", getParams()).call();
+		log.info("Memory in use after inputstream: " + memUsage());
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParser saxParser = factory.newSAXParser();
+		DefaultHandler handler = new DefaultHandler() {
+			ELinkResult result;
+			boolean isLinkSet;
+			boolean isDbFrom;
+			boolean isDbTo;
+			boolean isLinkName;
+			boolean isId;
+			String dbTo;
+			String linkName;
+			List<Long> idList;
+
+			public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+				if (qName.equalsIgnoreCase("LinkSet")) {
+					isLinkSet = true;
+					result = new ELinkResult();
+					relations.add(result);
+				} else if (qName.equalsIgnoreCase("DbFrom"))
+					isDbFrom = true;
+				else if (qName.equalsIgnoreCase("Id"))
+					isId = true;
+				else if (qName.equalsIgnoreCase("LinkSetDb")) {
+					isLinkSet = false;
+					dbTo = "";
+					linkName = "";
+					idList = new ArrayList();
+				} else if (qName.equalsIgnoreCase("DbTo"))
+					isDbTo = true;
+				else if (qName.equalsIgnoreCase("LinkName"))
+					isLinkName = true;
+			}
+
+			public void endElement(String uri, String localName, String qName) throws SAXException {
+				if (qName.equalsIgnoreCase("LinkSetDb")) {
+					result.setIds(dbTo, linkName, idList);
+				}
+			}
+
+			public void characters(char ch[], int start, int length) throws SAXException {
+				String text = new String(ch, start, length);
+				if (isDbFrom) {
+					result.setDbFrom(text);
+					isDbFrom = false;
+				} else if (isLinkSet && isId) {
+					result.setId(Long.parseLong(text));
+					isLinkSet = false;
+					isId = false;
+				} else if (isDbTo) {
+					dbTo = text;
+					isDbTo = false;
+				} else if (isId) {
+					idList.add(Long.parseLong(text));
+					isId = false;
+				} else if (isLinkName) {
+					linkName = text;
+					isLinkName = false;
+				}
+			}
+		};
+		log.info("Memory in use before parsing: " + memUsage());
+		log.info("Parsing elink results using SAX.");
+		saxParser.parse(is, handler);
+		log.info("Finished parsing elink results.");
+		log.info("Memory in use after parsing: " + memUsage());
+		return relations;
+	}
+	
+	private long memUsage() {
+		long mem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+		return mem / 1024 / 1024;
 	}
 
 	public void setDbFrom(String dbFrom) {
@@ -127,4 +261,7 @@ public class ELinkWebSession extends WebSessionBase {
 		this.linkName = linkName;
 	}
 
+	public void setTerm(String term) {
+		this.term = term;
+	}
 }
