8ea992de239283866607107bddf8890f4e982162
southern <southern@scripps.edu>
2012-10-16 14:08:00 +0000

    updated eutils and pubchem (axis2)

--- PubChemDB/PubChemDB-web/src/test/java/edu/scripps/fl/test/pubchem/test/IdentitySearchTester.java
+++ PubChemDB/PubChemDB-web/src/test/java/edu/scripps/fl/test/pubchem/test/IdentitySearchTester.java
@@ -1,21 +1,13 @@
 package edu.scripps.fl.test.pubchem.test;
 
-import java.util.Arrays;
-
 import edu.scripps.fl.pubchem.web.pug.PUGSoapFactory;
-
-
 
 public class IdentitySearchTester {
 
 	public static void main(String[] args) throws Exception {
 		int[] cids = PUGSoapFactory.getInstance().identitySearch("CCCCC", 1000, 1000);
+		System.out.println(String.format("Found %s CIDs", cids.length));
 		for(int cid: cids)
 			System.out.println(cid);
-		System.out.println("here");
-	}
-
-	
-
-	
+	}	
 }
